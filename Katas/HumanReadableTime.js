e602eeb6e632b5e9ed4142bc3f0a8e8d1e4ee57f
danyaobertan <danyaobertan@gmail.com>
2020-06-12 18:12:37 +0300

    Few cool Katas

---
+++ Katas/HumanReadableTime.js
@@ -0,0 +1,31 @@
+/*
+Write a function, which takes a non-negative integer (seconds) as input and returns the time in a human-readable format (HH:MM:SS)
+HH = hours, padded to 2 digits, range: 00 - 99
+MM = minutes, padded to 2 digits, range: 00 - 59
+SS = seconds, padded to 2 digits, range: 00 - 59
+The maximum time never exceeds 359999 (99:59:59)
+You can find some examples in the test fixtures.
+*/
+
+function humanReadable(seconds) {
+    let hours = (seconds - seconds % 3600) / 3600;
+    let minutes = ((seconds - hours * 3600) - seconds % 60) / 60;
+    seconds = seconds - (hours * 3600) - (minutes * 60);
+    if (hours < 10) {
+        hours = `0${hours}`
+    }
+    if (minutes < 10) {
+        minutes = `0${minutes}`
+    }
+    if (seconds < 10) {
+        seconds = `0${seconds}`
+    }
+    return `${hours}:${minutes}:${seconds}`
+}
+
+
+console.log(humanReadable(0), '00:00:00', 'humanReadable(0)');
+console.log(humanReadable(5), '00:00:05', 'humanReadable(5)');
+console.log(humanReadable(60), '00:01:00', 'humanReadable(60)');
+console.log(humanReadable(86399), '23:59:59', 'humanReadable(86399)');
+console.log(humanReadable(359999), '99:59:59', 'humanReadable(359999)');
